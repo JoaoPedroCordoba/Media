7a2f032617aacbf17cd785f7de11caaa032d1832
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-20 17:33:40 -0300

    finalizado exercicio Media aritimetica

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double n1, n2, n3, Media;
+
+Console.WriteLine("Digite um numero, Por Favor:");
+n1 = Convert.ToDouble(Console.ReadLine())!;
+
+Console.WriteLine("Digite o segundo numero, Por favor:");
+n2 = Convert.ToDouble(Console.ReadLine())!;
+
+Console.WriteLine("Digite o terceiro numero, Por favor:");
+n3 = Convert.ToDouble(Console.ReadLine());
+
+Media = (n1+n2+n3) / 3;
+
+Console.WriteLine($"A media é:{Media:N1}");
